03e4a726897d5656bf40280ca47a4515e1faa98e
Serg <korobko_ss@ukr.net>
2022-09-27 12:12:19 +0300

    add static data

---
+++ data/depts.js
@@ -0,0 +1,40 @@
+let depts = [
+     {
+         id:1,       
+         electricity: 'Електричество',
+         electricity_sum: 1234,
+         electricity_date: '21.05.2022',     
+         apartment: 'ОСББ',
+         apartment_sum: 497,
+         apartment_date: '22.05.2022',     
+         water: 'Вода',
+         water_sum: 124,
+         water_date: '23.05.2022'
+      },
+      {
+         id:2,       
+         electricity: 'Електричество',
+         electricity_sum: 1231,
+         electricity_date: '21.05.2022',     
+         apartment: 'ОСББ',
+         apartment_sum: 34,
+         apartment_date: '22.05.2022',     
+         water: 'Вода',
+         water_sum: 234,
+         water_date: '23.05.2022'
+      },
+      {
+         id:3,       
+         electricity: 'Електричество',
+         electricity_sum: 567,
+         electricity_date: '21.05.2022',     
+         apartment: 'ОСББ',
+         apartment_sum: 58,
+         apartment_date: '22.05.2022',     
+         water: 'Вода',
+         water_sum: 322,
+         water_date: '23.05.2022'
+      },
+]
+
+module.exports = depts
---
+++ data/users.js
@@ -0,0 +1,25 @@
+let users = [
+   {
+      id:1,
+      name: 'Sergey',
+      surname: 'Korobko',
+      email: 'korobko_ss@ukr.net',
+      password: '12Serg0591',  
+   },
+   {
+      id:2,
+      name: 'Lera',
+      surname: 'Korobko',
+      email: 'valer@ukr.net',
+      password: 'valera123'
+   },
+   {
+      id:3,
+      name: 'Dmitriy',
+      surname: 'Prihodko',
+      email: 'alex@ukr.net',
+      password: '123alex!'
+   },
+]
+
+module.exports = users
